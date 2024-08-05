5623c64e52e278896cd1d65583e3b108d0fc644f
Risav03 <needleweed03@gmail.com>
2024-08-05 23:54:03 +0530

    fix email

--- components/Home/FooterComponent.tsx
+++ components/Home/FooterComponent.tsx
@@ -8,8 +8,8 @@ const FooterComponent = () => {
         <h2 className='text-2xl font-bold'> Nifty Tales </h2>
         <div className='grid grid-cols-3 max-md:grid-cols-2 max-md:text-sm max-md:mt-5 mt-10'>
             <ul className='flex flex-col gap-2'>
-                <li className='max-md:hidden'><a className='flex items-center justify-center gap-2 w-fit' href="https://mail.google.com/mail/?view=cm&fs=1&to=risavdeb03@gmail.com" target='_blank'><IoMdMail/>Email</a></li>
-                <li className='md:hidden'><a className='flex items-center justify-center gap-2 w-fit' href="https://mail.google.com/mail/?view=cm&fs=1&to=risavdeb03@gmail.com" target='_blank'><IoMdMail/>Email</a></li>
+                <li className='max-md:hidden'><a className='flex items-center justify-center gap-2 w-fit' href="https://mail.google.com/mail/?view=cm&fs=1&to=helloniftytales@gmail.com" target='_blank'><IoMdMail/>Email</a></li>
+                <li className='md:hidden'><a className='flex items-center justify-center gap-2 w-fit' href="https://mail.google.com/mail/?view=cm&fs=1&to=helloniftytales@gmail.com" target='_blank'><IoMdMail/>Email</a></li>
                 {/* <li>Instagram</li> */}
                 <li><a href="https://x.com/niftytales" className='flex items-center justify-center gap-2 w-fit' target='_blank'> <FaTwitter className='text-white'/> Twitter</a></li>
                 {/* <li>Discord</li> */}
