2b0f321de61995b66cd84960403b9aa13d352efd
Risav03 <needleweed03@gmail.com>
2024-08-05 20:20:16 +0530

    gooo

--- app/beta/makeAuthor/page.tsx
+++ app/beta/makeAuthor/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React , {useState} from "react"
+import React , {useState, useRef} from "react"
 import { ethers } from "ethers";
 import abi from "@/utils/abis/templateABI"
 import { bytecode } from "@/utils/bytecode/bytecode";
@@ -40,16 +40,36 @@ export default function Home(){
         }
       }
 
+      const fileInputRef = useRef(null);
+
+        const handleClick = () => {
+
+            //@ts-ignore
+            fileInputRef.current.click();
+        };
+
 
     return(
         <div className="flex flex-col items-center justify-start w-screen min-h-screen p-10">
             <WalletConnectButton/>
+
             <h2 className="text-sm">Collection Name</h2>
             <input onChange={(e)=>{setCollectionName(e.target.value)}} value={collectionName} className="p-2 rounded-xl border-2 border-black" ></input>
 
             <h2 className="text-sm mt-5">Collection Symbol</h2>
             <input onChange={(e)=>{setSymbol(e.target.value)}} value={symbol} className="p-2 rounded-xl border-2 border-black" ></input>
 
+            <h2 className="text-sm mt-5">Upload a Photo</h2>
+
+            <button onClick={handleClick} className="w-40 h-40 rounded-xl border-4 border-gray-600 bg-gray-400" ></button>
+            <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            accept="image/png, image/jpeg, image/jpg"
+            //@ts-ignore
+            onChange={(e : React.ChangeEvent<HTMLInputElement>) => console.log(e.target.files)}
+      />
             <button onClick={deployContract} className="bg-black text-white px-4 py-2 rounded-xl my-10 hover:scale-105 duration-200" >Create</button>
         </div>
     )
