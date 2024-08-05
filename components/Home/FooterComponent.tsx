de717433ad884ee0d0e9c471585f18d927f0f356
Risav03 <needleweed03@gmail.com>
2024-08-05 23:50:15 +0530

    fixed footer

--- components/Home/FooterComponent.tsx
+++ components/Home/FooterComponent.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { FaTwitter } from 'react-icons/fa'
+import { IoMdMail } from 'react-icons/io'
 
 const FooterComponent = () => {
   return (
     <div className='bg-[#1a1a1a] w-screen px-20 pt-20 max-md:px-5 max-md:pt-10 text-white'>
         <h2 className='text-2xl font-bold'> Nifty Tales </h2>
         <div className='grid grid-cols-3 max-md:grid-cols-2 max-md:text-sm max-md:mt-5 mt-10'>
-            <ul className='flex flex-col'>
-                <li className='max-md:hidden'><a href="mailto:helloniftytales@gmail.com" target='_blank'>helloniftytales@gmail.com</a></li>
-                <li className='md:hidden'><a href="mailto:helloniftytales@gmail.com" target='_blank'>Email</a></li>
+            <ul className='flex flex-col gap-2'>
+                <li className='max-md:hidden'><a className='flex items-center justify-center gap-2 w-fit' href="https://mail.google.com/mail/?view=cm&fs=1&to=risavdeb03@gmail.com" target='_blank'><IoMdMail/>Email</a></li>
+                <li className='md:hidden'><a className='flex items-center justify-center gap-2 w-fit' href="https://mail.google.com/mail/?view=cm&fs=1&to=risavdeb03@gmail.com" target='_blank'><IoMdMail/>Email</a></li>
                 {/* <li>Instagram</li> */}
                 <li><a href="https://x.com/niftytales" className='flex items-center justify-center gap-2 w-fit' target='_blank'> <FaTwitter className='text-white'/> Twitter</a></li>
                 {/* <li>Discord</li> */}
