62181b41eb64d0bb3ecbf5b97f2f3ded5bab1693
Risav03 <needleweed03@gmail.com>
2024-08-09 14:25:57 +0530

    changes

--- app/authors/[contractAdd]/page.tsx
+++ app/authors/[contractAdd]/page.tsx
@@ -63,14 +63,19 @@ export default function Home(){
     },[address])
 
     return(
-        <div>
+        <div className="">
             <Navbar/>
-            <div className="w-screen h-80 overflow-hidden object-fill mt-16 relative">
+            <div className="w-screen h-[20rem] overflow-hidden object-fill mt-16 relative">
                 <Image width={1080} height={1080} src={bucketLink} alt="dp" className="w-[150vw] absolute blur-xl translate-y-[-40rem] rounded-full" />
                 <div className="flex gap-4 items-center absolute z-50 top-[4.5rem] left-10">
                     <Image width={1080} height={1080} src={bucketLink} alt="dp" className="w-[10rem] h-[10rem] border-4 border-white rounded-full" />
                     <h2 className="text-5xl font-bold text-white">{name}</h2>
                 </div>
+            </div>
+
+            <div className="w-screen h-[25rem] flex items-center justify-center flex-col">
+                <h2 className="text-xl font-bold">Publish your first book!</h2>
+                <button className='bg-[#000000] rounded-lg hover:-translate-y-1 duration-200 text-[#eeeeee] h-10 font-semibold flex items-center justify-center gap-2 px-5 w-52 my-2 max-md:mx-auto'>Publish</button>
             </div>
         </div>
     )
