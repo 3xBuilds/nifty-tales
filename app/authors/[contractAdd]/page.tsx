afee2af04773135dc8fcd3aed9e523c2d02e3919
Risav03 <needleweed03@gmail.com>
2024-08-11 23:43:20 +0530

    push

--- app/authors/[contractAdd]/page.tsx
+++ app/authors/[contractAdd]/page.tsx
@@ -8,13 +8,21 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { WalletConnectButton } from "@/components/buttons/WalletConnectButton";
 import Navbar from "@/components/Home/Navbar";
+import { useGlobalContext } from "@/context/MainContext";
+import { useRouter } from "next/navigation";
 
 export default function Home(){
 
     const pathName = usePathname();
     const {address} = useAccount();
 
-    const[bucketLink, setBucketLink] = useState<string>("")
+    const router = useRouter()
+
+    const {user} = useGlobalContext();
+
+    const[profileImgLink, setProfileImgLink] = useState<string>("")
+    const[bannerLink, setBannerLink] = useState<string>("")
+
     const[name, setName] = useState<string>("")
 
     async function contractSetup(){
@@ -29,7 +37,8 @@ export default function Home(){
                 const provider = new ethers.providers.Web3Provider(window.ethereum);
                 const signer = provider.getSigner();
 
-                const contract = new ethers.Contract(pathName.split("/")[2], abi, signer);
+                //@ts-ignore
+                const contract = new ethers.Contract(pathname.split("/")[2], abi, signer);
 
             return contract;
 
@@ -46,6 +55,8 @@ export default function Home(){
             const contract = await contractSetup();
             const contractName = await contract?.name();
 
+            console.log(contractName);
+
             setName(contractName);
         }
         catch(err){
@@ -55,25 +66,30 @@ export default function Home(){
 
     useEffect(()=>{
         if(address){
-            setBucketLink("https://nifty-tales.s3.ap-south-1.amazonaws.com/users/" + address + "/info/profileImage");
+            setProfileImgLink("https://nifty-tales.s3.ap-south-1.amazonaws.com/users/" + address + "/info/profileImage");
+            setBannerLink("https://nifty-tales.s3.ap-south-1.amazonaws.com/users/" + address + "/info/bannerImage");
             getContractDetails();
         }
-    },[address])
+    },[user])
 
     return(
         <div className="">
-            <Navbar/>
-            <div className="w-screen h-[20rem] overflow-hidden object-fill mt-16 relative">
-                <Image width={1080} height={1080} src={bucketLink} alt="dp" className="w-[150vw] absolute blur-xl translate-y-[-40rem] rounded-full" />
-                <div className="flex gap-4 items-center absolute z-50 top-[4.5rem] left-10">
-                    <Image width={1080} height={1080} src={bucketLink} alt="dp" className="w-[10rem] h-[10rem] border-4 border-white rounded-full" />
-                    <h2 className="text-5xl font-bold text-white">{name}</h2>
+            <div className="h-16 w-screen">
+                <Navbar/>
+            </div>
+            <div className="w-screen relative h-[10rem] md:h-[22rem] max-md:flex items-center justify-center overflow-hidden object-fill ">
+                <div className="w-screen absolute h-full overflow-hidden">
+                    <Image width={1080} height={1080} src={bannerLink} alt="dp" className="w-full h-full object-cover object-center absolute top-1/2 left-1/2 transform -translate-x-1/2 brightness-50 -translate-y-1/2"/>
+                </div>
+                <div className="flex gap-8 object-center items-center h-full justify-center my-auto max-md:w-[90%] absolute z-50  md:left-10">
+                    <Image width={1080} height={1080} src={profileImgLink} alt="dp" className="md:w-[10rem] md:h-[10rem] h-[6rem] w-[6rem] border-4 border-white rounded-full" />
+                    <h2 className="md:text-5xl text-xl font-bold text-white">{name}</h2>
                 </div>
             </div>
 
             <div className="w-screen h-[25rem] flex items-center justify-center flex-col">
                 <h2 className="text-xl font-bold">Publish your first book!</h2>
-                <button className='bg-[#000000] rounded-lg hover:-translate-y-1 duration-200 text-[#eeeeee] h-10 font-semibold flex items-center justify-center gap-2 px-5 w-52 my-2 max-md:mx-auto'>Publish</button>
+                <button onClick={()=>{router.push("/publish")}} className='bg-[#000000] rounded-lg hover:-translate-y-1 duration-200 text-[#eeeeee] h-10 font-semibold flex items-center justify-center gap-2 px-5 w-52 my-2 max-md:mx-auto'>Publish</button>
             </div>
         </div>
     )
