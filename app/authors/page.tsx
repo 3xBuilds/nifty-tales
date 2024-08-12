ba781acc5bd6f8c0aa1aae7201f0bbb17480d236
Risav03 <needleweed03@gmail.com>
2024-08-13 01:59:16 +0530

    push

--- app/authors/page.tsx
+++ app/authors/page.tsx
@@ -149,7 +149,10 @@ export default function Home(){
                 </div>
                 <div className="flex gap-8 object-center items-center h-full md:px-10 w-screen justify-center md:justify-start my-auto absolute z-50 backdrop-blur-xl">
                     <Image width={1080} height={1080} src={profileImgLink || ""} alt="dp" className="md:w-[10rem] md:h-[10rem] h-[6rem] w-[6rem] border-4 border-white rounded-full" />
-                    <h2 className="md:text-5xl text-xl font-bold text-white">{name}</h2>
+                    <div className="flex flex-col gap-2">
+                        <h2 className="md:text-5xl text-xl font-bold text-white">{name}</h2>
+                        <a href={`https://polygonscan.com/address/${user?.contractAdd}`} className="md:text-md text-sm underline font-semibold text-white">{user?.contractAdd.substring(0,7)}...{user?.contractAdd.substring(user.contractAdd.length-7, user.contractAdd.length)}</a>
+                    </div>
                 </div>
             </div>
             
