dcd37e35fb9ca113e803ff4537112e123848739a
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-08-12 13:00:51 +0530

    book get route added

---
+++ app/api/book/route.ts
@@ -0,0 +1,46 @@
+import Book from "@/schemas/bookSchema"
+import { connectToDB } from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server"
+
+type BookType = {
+    name: string;
+    isPublished?: boolean;
+    price?: number;
+    maxMint?: number;
+    cover?: string | null;
+    author: Object | null;
+    artist?: string | null;
+    ISBN?: string | null;
+    description?: string | null;
+    tags?: string[];
+    pdf: string;
+    readers?: number;
+    isBoosted?: string | null;
+    createdAt?: Date;
+}
+
+// get all books route
+export async function GET(req: NextRequest) {
+
+    try {
+        await connectToDB();
+        const books: BookType[] = await Book.find();
+
+        if (books.length === 0) {
+            return NextResponse.json({
+                data: [],
+                message: "No books found"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json({
+            data: books
+        }, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json({
+            message: "Something went wrong"
+        }, { status: 500 })
+    }
+
+}
