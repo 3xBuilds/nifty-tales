1c2b994a299284f56985424079d7af5c409fd186
Risav03 <needleweed03@gmail.com>
2024-08-23 19:09:27 +0530

    push

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -4,12 +4,22 @@ import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import { useEffect, useState } from 'react';
 
 
 export default function Home(){
+
+    const[wallet, setWallet] = useState("")
+    const[id, setId] = useState<string>("")
+
+    useEffect(()=>{
+        setWallet(localStorage.getItem('address') || "");
+        setId(localStorage.getItem('id') || "")
+    },[])
+
     return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <Viewer fileUrl={`https://nifty-tales.s3.ap-south-1.amazonaws.com/users/${localStorage.getItem('address')}/content/${localStorage.getItem('id')}/book`} />
+        <Viewer fileUrl={`https://nifty-tales.s3.ap-south-1.amazonaws.com/users/${wallet}/content/${id}/book`} />
     </Worker>    
     );
 
