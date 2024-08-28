0b9e26841bfde616c682322fd8e5907e9c4fada5
Risav03 <needleweed03@gmail.com>
2024-08-28 17:18:09 +0530

    go

--- app/authors/[wallet]/page.tsx
+++ app/authors/[wallet]/page.tsx
@@ -14,25 +14,23 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   // read route params
   const id = params.wallet
-  // console.log(id);
 
   const user = await fetch(`https://niftytales.xyz/api/user/wallet/${id}`).then((res) => res.json())
-  console.log("BOOK DATA",user);
  
   return {
-    title: user?.user.collectionName,
+    title: user?.user?.collectionName,
     openGraph: {
-      title: user?.user.collectionName,
+      title: user?.user?.collectionName,
       url: `https://niftytales.xyz/authors/${id}`,
       siteName: 'Nifty Tales',
       images: [
         {
-          url: `https://nifty-tales.s3.ap-south-1.amazonaws.com/users/${id}"/info/profileImage`, // Must be an absolute URL
+          url: `https://nifty-tales.s3.ap-south-1.amazonaws.com/users/${id}/info/profileImage`, // Must be an absolute URL
           width: 800,
           height: 600,
         },
         {
-          url: `https://nifty-tales.s3.ap-south-1.amazonaws.com/users/${id}"/info/profileImage`, // Must be an absolute URL
+          url: `https://nifty-tales.s3.ap-south-1.amazonaws.com/users/${id}/info/profileImage`, // Must be an absolute URL
           width: 1800,
           height: 1600,
           alt: 'My custom alt',
