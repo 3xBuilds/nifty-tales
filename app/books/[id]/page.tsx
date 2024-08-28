40a2f887dfb50518253151329f04a02940b6fe52
Risav03 <needleweed03@gmail.com>
2024-08-28 16:31:59 +0530

    go

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -16,16 +16,34 @@ export async function generateMetadata(
   // console.log(id);
 
   const book = await fetch(`https://niftytales.vercel.app/api/book/${id}`).then((res) => res.json())
-  console.log("BOOK DATA",book.data);
+  // console.log("BOOK DATA",book.data);
  
   return {
     title: book?.data?.name,
     openGraph: {
-      images: [book?.data?.cover],
-    },
+      title: book?.data?.name,
+      description: 'Empowering Authors, Engaging Readers',
+      url: `https://niftytales.vercel.app/books/${id}`,
+      siteName: 'Nifty Tales',
+      images: [
+        {
+          url: book.data.cover, // Must be an absolute URL
+          width: 800,
+          height: 600,
+        },
+        {
+          url: book.data.cover, // Must be an absolute URL
+          width: 1800,
+          height: 1600,
+          alt: 'My custom alt',
+        },
+        
+      ],
+      locale: 'en_US',
+    type: 'website',
   }
 }
-
+}
  
 export default function Page() {
     return (
