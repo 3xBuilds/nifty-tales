65e2d3aabe41acf05f40678456d0983760cc138f
Risav03 <needleweed03@gmail.com>
2024-09-05 23:27:56 +0530

    added signout with connect this wallet thingy

--- components/popups/walletNotRegistered.tsx
+++ components/popups/walletNotRegistered.tsx
@@ -1,16 +1,21 @@
 "use client"
 
 import { useGlobalContext } from '@/context/MainContext'
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export const WalletNotRegistered = () => {
 
     const {user} = useGlobalContext();
+    const router = useRouter()
 
   return (
     <div className='w-screen h-screen backdrop-blur-2xl fixed bg-black/50 text-white top-0 left-0 z-[1000] flex flex-col items-center justify-center '>
         <h2 className='text-xl font-bold'>You are connected with an unregistered wallet!</h2>
         <h3 className='text-lg font-semibold mt-10' >Connect {user?.wallet} to continue.</h3>
+        <h3 className='my-4'>OR</h3>
+        <button className='text-black w-32 h-10 bg-gray-300' onClick={()=>{signOut(); router.push("/connect")}}>Sign Out</button>
     </div>
   )
 }
