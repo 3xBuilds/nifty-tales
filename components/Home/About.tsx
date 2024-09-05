89105915311ce2d7f99d5269c02c1e10d1ca3d6c
Risav03 <needleweed03@gmail.com>
2024-09-06 00:41:30 +0530

    changed page

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const About = () => {
   return (
     <div className=' text-center my-20'>
-        <h2 className='text-4xl max-md:text-3xl mt-5 font-bold'>About Us</h2>
+        {/* <h2 className='text-4xl max-md:text-3xl mt-5 font-bold'>About Us</h2> */}
         <h3 className='text-3xl max-md:text-xl mt-20 font-bold'>What is Nifty Tales?</h3>
-        <h2 className='text-xl max-md:text-base max-md:w-[80%] text-gray-500 font-light w-[60%] mx-auto mt-10'>Nifty Tales is revolutionizing digital literature by connecting authors and readers through blockchain technology. Our platform allows authors to upload their books as NFTs, providing free access to readers worldwide. Readers can support their favorite authors by minting books they love. At Nifty Tales, we're creating a vibrant community where creativity flourishes and great stories are accessible to all. Join us in shaping the future of digital reading and publishing.</h2>
+        <h2 className='text-lg max-md:text-base max-md:w-[80%] text-gray-500 font-light w-[60%] mx-auto mt-10'>Nifty Tales is a digital platform where authors publish stories as NFTs, allowing readers to enjoy free reading or mint stories for cultural support.</h2>
         <h3 className='text-3xl max-md:text-xl mt-20 font-bold'>Our Vision</h3>
-        <h2 className='text-xl max-md:text-base max-md:w-[80%] text-gray-500 font-light w-[60%] mx-auto mt-10'>Our vision is to revolutionize storytelling by creating a dynamic space where creativity thrives. We empower authors to monetize their work and offer readers unique, collectible editions. We aim to make storytelling accessible and engaging for everyone.</h2>
+        <h2 className='text-lg max-md:text-base max-md:w-[80%] text-gray-500 font-light w-[60%] mx-auto mt-10'>We aim to revolutionize storytelling by merging literature with blockchain, empowering authors and creating a space for readers to engage and support.</h2>
         <h3 className='text-3xl max-md:text-xl mt-20 font-bold'>Our Team</h3>
-        <h2 className='text-xl max-md:text-base max-md:w-[80%] text-gray-500 font-light w-[60%] mx-auto mt-10'>Our team is a passionate group of professionals from blockchain, digital art, and publishing. United by our love for books, storytelling, and solving problems, we are dedicated to transforming the literary landscape with innovation and creativity.</h2>
+        <h2 className='text-lg max-md:text-base max-md:w-[80%] text-gray-500 font-light w-[60%] mx-auto mt-10'>Nifty Tales offers a unique blend of free access to unlimited stories and the opportunity to support authors by minting their work as NFTs, making it easy to engage with great content and support creativity.</h2>
         {/* <div className='flex items-center justify-center my-20 gap-10'>
             <div className='w-72 hover:scale-110 duration-150 rounded-xl bg-[#fafafa] shadow-black/25 shadow-lg p-5 flex flex-col items-center justify-center'>
                 <div className='w-[95%] bg-white overflow-hidden aspect-square rounded-full'></div>
