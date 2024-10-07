02e633175eefe2e9a0af9dd42849c18050dc1c57
im-still-thinking <royaritra1990rocks@gmail.com>
2024-10-07 23:25:47 +0530

    deployment update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     images: {
         domains: ['nifty-tales.s3.amazonaws.com'],
         unoptimized: true,
